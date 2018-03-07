8fca329e400ea8cd0ac5bb311df32d370dadfdc4
uncarman <34087593@qq.com>
2018-03-07 22:00:27 +0800

    大首页增加中心权限校验功能

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -3,7 +3,7 @@
     <div class="menuContent">
       <ul v-for="(centerRouter, index) in centerRouters" :key="index">
         <li v-for="(row, index) in centerRouter" :key="index">
-          <a @click="validateToken(row.url)">
+          <a @click="clickPlatformId = row.platformId; validateToken(row.url)">
             <img :src="row.imgSrc" />
             <p>{{row.name}}</p>
           </a>
@@ -45,27 +45,29 @@
       return {
         isActive: false,
         userInfo: {},
+        clickPlatformId: -1,
+        platformIds: [],
         centerRouters: [
           [
-            {url: `${this.getBasePath(process.env.env).basePath}:8100/`, imgSrc: 'static/img/menu/sales_center.png', name: '销售中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:8106/`, imgSrc: 'static/img/menu/product_center.png', name: '产品中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:8105/`, imgSrc: 'static/img/menu/supporter_manage_center.png', name: '供应商管理中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:8103/`, imgSrc: 'static/img/menu/foreign_service_center.png', name: '客服中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:8101/`, imgSrc: 'static/img/menu/foreign_employee_center.png', name: '雇员中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8100/`, imgSrc: 'static/img/menu/sales_center.png', platformId: '1', name: '销售中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8106/`, imgSrc: 'static/img/menu/product_center.png', platformId: '11', name: '产品中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8105/`, imgSrc: 'static/img/menu/supporter_manage_center.png', platformId: '14', name: '供应商管理中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8103/`, imgSrc: 'static/img/menu/foreign_service_center.png', platformId: '3', name: '客服中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8101/`, imgSrc: 'static/img/menu/foreign_employee_center.png', platformId: '5', name: '雇员中心'},
           ],
           [
-            {url: `${this.getBasePath(process.env.env).basePath}:8108/#/main/`, imgSrc: 'static/img/menu/foreign_support_center.png', name: '外企支持中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:8109/#/`, imgSrc: 'static/img/menu/proxy_center.png', name: '代理中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:8107/#/`, imgSrc: 'static/img/menu/foreign_control_center.png', name: '外企内控中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:8112/#/`, imgSrc: 'static/img/menu/finance_advisory_business_center.png', name: '财务咨询业务中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:8113/#/`, imgSrc: 'static/img/menu/finance_advisory_operator_center.png', name: '财务咨询运营中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8108/#/main/`, imgSrc: 'static/img/menu/foreign_support_center.png', platformId: '6', name: '外企支持中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8109/#/`, imgSrc: 'static/img/menu/proxy_center.png', platformId: '4', name: '代理中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8107/#/`, imgSrc: 'static/img/menu/foreign_control_center.png', platformId: '2', name: '外企内控中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8112/#/`, imgSrc: 'static/img/menu/finance_advisory_business_center.png', platformId: '7', name: '财务咨询业务中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8113/#/`, imgSrc: 'static/img/menu/finance_advisory_operator_center.png', platformId: '9', name: '财务咨询运营中心'},
           ],
           [
-            {url: `${this.getBasePath(process.env.env).basePath}:8104/`, imgSrc: 'static/img/menu/service_outsourcing_business_center.png', name: '服务外包业务中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:6110/#/`, imgSrc: 'static/img/menu/bill_center.png', name: '账单中心'},
-            {url: `${this.getBasePath(process.env.env).basePath}:6111/#/`, imgSrc: 'static/img/menu/settlement_center.png', name: '结算中心'},
-            {url: '#', imgSrc: 'static/img/menu/finance_advisory_report_center.png', name: '报表中心'},
-            {url: '#', imgSrc: 'static/img/menu/business_intelligence_center.png', name: '商业智能中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:8104/`, imgSrc: 'static/img/menu/service_outsourcing_business_center.png', platformId: '10', name: '服务外包业务中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:6110/#/`, imgSrc: 'static/img/menu/bill_center.png', platformId: '12', name: '账单中心'},
+            {url: `${this.getBasePath(process.env.env).basePath}:6111/#/`, imgSrc: 'static/img/menu/settlement_center.png', platformId: '13', name: '结算中心'},
+            {url: '#', imgSrc: 'static/img/menu/finance_advisory_report_center.png', platformId: '8', name: '报表中心'},
+            {url: '#', imgSrc: 'static/img/menu/business_intelligence_center.png', platformId: '15', name: '商业智能中心'},
           ],
         ],
         taskList: [
@@ -103,14 +105,44 @@
         param.append("token", that.userInfo.token);
         axios({
           method: "POST",
-          url: `${this.getBasePath(process.env.env).serverPath}/api/getUserInfoByToken`,
+          url: `${this.getBasePath(process.env.env).serverPath}:9621/api/getUserInfoByToken`,
           data: param,
         }).then(response => {
           if(response.data.code !== 0) {
             this.backToLogin();
             return;
           } else {
-            that.setCrossToken(url)
+            this.getMenuAuth(url);
+          }
+        })
+      },
+      getMenuAuth(url) {
+        axios({
+          method: "GET",
+          url: `${this.getBasePath(process.env.env).serverPath}:2003/api/authservice/auth/getPlatformAuth/${this.userInfo.userId}`,
+        }).then(response => {
+          if(response.data.code !== 0) {
+            this.backToLogin();
+            return;
+          } else {
+            this.platformIds = response.data.data.replace(/\[|\]/g, '').replace(/ /g,'').split(',');
+            if (this.platformIds && this.platformIds.length > 0) {
+              let isCanRoute = false;
+              for (let i = 0, len = this.platformIds.length; i < len; i++) {
+                if (this.platformIds[i] === this.clickPlatformId) {
+                  isCanRoute = true;
+                }
+              }
+              if (isCanRoute) {
+                this.setCrossToken(url);
+              } else {
+                this.$Message.error('没有权限~');
+                return;
+              }
+            } else {
+              this.$Message.error('没有权限~');
+              return;
+            }
           }
         })
       },
@@ -171,7 +203,7 @@
             originReg = /localhost:.*$/;
             break
         }
-        return {basePath: basePath, serverPath: `${serverPath}:9621`, originReg: originReg}
+        return {basePath: basePath, serverPath: `${serverPath}`, originReg: originReg}
       },
       openMessageBox() {
         this.$Notice.open({
